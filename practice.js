ba6f70550b6f88cd63a41f20615a544f32b3154c
MD Ikramul Kayes <md.ikramul.kayes@g.bracu.ac.bd>
2021-09-22 22:17:36 +0600

    first js code

---
+++ practice.js
@@ -0,0 +1,7 @@
+var username = "kaYes";
+username = username.toLowerCase()
+var final;
+final = username.slice(0,1).toUpperCase();
+final =final + username.slice(1,username.length);
+
+console.log(final) /* printing statements */
