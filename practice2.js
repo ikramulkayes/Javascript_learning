a04158ab44b2fbf4280d8524c0fd4643bbab5139
MD Ikramul Kayes <md.ikramul.kayes@g.bracu.ac.bd>
2021-09-23 09:29:32 +0600

    Update practice2.js

--- practice2.js
+++ practice2.js
@@ -2,6 +2,10 @@ function lovecalculator(s1,s2){
     var n = Math.random()
     n = n*100 
     n = Math.floor(n)+1
-    return s1+"'s love for "+s2 +" is " +String(n) + "%"
-}
+    if (n>50){
+        return s1+"'s love for "+s2 +" is " +String(n) + "% .Good luck with your life"
+    }
+    else{
+        return s1+"'s love for "+s2 +" is " +String(n) + "%"
+}}
 console.log(lovecalculator("kamal","Cathreen"))
